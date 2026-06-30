323ac0f250e4e2b18906c52a13693e87c675a8bd
damma83 <damiano.nacchia@gmail.com>
2026-07-01 01:55:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from utils import send_telegram, extract_price
-from sites.unieuro import get_price
+from sites.unieuro import get_price as get_unieuro
+from sites.mediaworld import get_mediaworld_price
 
 TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
@@ -8,13 +9,22 @@ TARGET = float(os.environ.get("TARGET_PRICE", 900))
 
 PRODUCTS = [
     {
-        "name": "MacBook Air M4",
+        "name": "MacBook Air M4 (Unieuro)",
         "url": "https://www.unieuro.it/"
+    },
+    {
+        "name": "MacBook Air M4 (MediaWorld)",
+        "url": "https://www.mediaworld.it/it/brand/apple/mac/macbook/macbook-air-m4"
     }
 ]
 
 for p in PRODUCTS:
-    text = get_price(p["url"])
+
+    if "unieuro" in p["url"]:
+        text = get_unieuro(p["url"])
+    else:
+        text = get_mediaworld_price(p["url"])
+
     price = extract_price(text)
 
     if price:
